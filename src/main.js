3ee4271bfddd9019539012c4c69b584922347fda
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-12 17:24:48 -0300

    agrego página nueva /map

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import * as VueGoogleMaps from "vue2-google-maps";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY
+  },
+  installComponents: true
+});
 
 new Vue({
   router,
---
+++ src/views/Map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="map">
+    <GmapMap
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  components: {}
+};
+</script>
