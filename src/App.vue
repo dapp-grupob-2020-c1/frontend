0b1f8308bc329355ecab0d60655aeb5dca78c9a0
Tehuel <torresbaldi.tehuel@gmail.com>
2020-06-17 14:45:30 -0300

    adds footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <div id="app">
     <Navbar></Navbar>
     <router-view />
+    <div class="bg-light text-dark py-5 mt-5 text-center">
+      <p>Comprando En Casa</p>
+      <p>
+        <a href="https://github.com/dapp-grupob-2020-c1/frontend"
+          >Repo Frontend</a
+        >
+        |
+        <a href="https://github.com/dapp-grupob-2020-c1/backend"
+          >Repo Backend</a
+        >
+      </p>
+    </div>
   </div>
 </template>
 
