f7ca37828c9d738edbbc892df4c26bfe981a3baa
Tehuel <torresbaldi.tehuel@gmail.com>
2020-06-20 15:25:26 -0300

    lint fix

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="map" style="display: flex; justify-content: center; align-items: center">
+  <div
+    class="map"
+    style="display: flex; justify-content: center; align-items: center"
+  >
     <GmapMap
       :center="{ lat: -34.7058569, lng: -58.2772947 }"
       :zoom="15"
