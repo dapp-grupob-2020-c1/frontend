6e52436cec49c904185733446c4db1edcf6b5c56
Tehuel <torresbaldi.tehuel@gmail.com>
2020-06-20 15:57:03 -0300

    extract footer component

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,19 @@
   <div id="app">
     <Navbar></Navbar>
     <router-view />
-    <div class="bg-light text-dark py-5 mt-5 text-center">
-      <p>Comprando En Casa</p>
-      <p>
-        <a href="https://github.com/dapp-grupob-2020-c1/frontend"
-          >Repo Frontend</a
-        >
-        |
-        <a href="https://github.com/dapp-grupob-2020-c1/backend"
-          >Repo Backend</a
-        >
-      </p>
-    </div>
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import FooterComponent from "./components/FooterComponent";
+
 export default {
   name: "App",
   components: {
-    Navbar
+    Navbar,
+    FooterComponent
   }
 };
 </script>
