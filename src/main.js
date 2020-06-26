87efca6a35144fb14aaa0db18074b57a7724dbe9
Tehuel <torresbaldi.tehuel@gmail.com>
2020-06-25 22:44:35 -0300

    install vue-authenticate
    also reorder some imports in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
 import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "./assets/style.scss";
-
-import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false;
 
+// Vue-Authenticate
+import VueAuthenticate from "vue-authenticate";
+Vue.use(VueAxios, axios);
+Vue.use(VueAuthenticate, {
+  baseUrl: "http://localhost:3000", // Your API domain
+  providers: {
+    github: {
+      clientId: "",
+      redirectUri: "http://localhost:8080/auth/callback" // Your client app URL
+    }
+  }
+});
+
+// Vue Google Maps
+import * as VueGoogleMaps from "vue2-google-maps";
 Vue.use(VueGoogleMaps, {
   load: {
     key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY
@@ -16,9 +31,9 @@ Vue.use(VueGoogleMaps, {
   installComponents: true
 });
 
-// Install BootstrapVue
+// BootstrapVue
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
 new Vue({
