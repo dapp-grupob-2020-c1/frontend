6450344b01ecc2bd27c4a6709d3f5cf9bfab1c51
Tehuel <torresbaldi.tehuel@gmail.com>
2020-06-28 20:16:38 -0300

    basic dashboard

---
+++ src/components/CustomerLocationsCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-card title="Ubicaciones">
+    <p>
+      Las direcciones cargadas por el cliente son usadas para calcular los
+      envíos de productos.
+    </p>
+    <b-list-group v-if="locations.length">
+      <b-list-group-item v-for="(location, index) in locations" :key="index">
+        {{ location }}
+      </b-list-group-item>
+    </b-list-group>
+    <b-alert variant="info" show>
+      No hay ninguna ubicación. Cargar.
+    </b-alert>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "CustomerLocationsCard",
+  computed: {
+    locations() {
+      return this.$store.state.customer.locations;
+    }
+  },
+  methods: {}
+};
+</script>
