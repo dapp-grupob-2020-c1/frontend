b3ed0789c3fe6c4d7246b0f2b95720755bfc7042
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-04 17:55:36 -0300

    translate Register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,20 +2,21 @@
   <b-container class="my-5">
     <b-row align-h="center" align-v="center">
       <b-col cols="12" md="8" lg="6">
-        <h1>Registrar Nuevo Usuario</h1>
+        <h1>{{ $t("register.register_new_user") }}</h1>
         <b-card no-body>
           <b-tabs card content-class="mt-3">
-            <b-tab title="Comprador" active>
+            <b-tab :title="$t('register.buyer')" active>
               <RegisterBuyerForm />
             </b-tab>
-            <b-tab title="Vendedor">
+            <b-tab :title="$t('register.seller')">
               <RegisterShopForm />
             </b-tab>
           </b-tabs>
         </b-card>
         <p>
-          Ya tenés cuenta?
-          <router-link to="/login">Iniciar Sesión</router-link>.
+          {{ $t("register.already_have_account") }}
+          <router-link to="/login">{{ $t("register.login") }}</router-link
+          >.
         </p>
       </b-col>
     </b-row>
