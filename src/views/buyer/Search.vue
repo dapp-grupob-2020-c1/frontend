91d2ca07730514b2fd9a8f773cd4a3129eb0d730
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-04 19:46:21 -0300

    translate search page

--- src/views/buyer/Search.vue
+++ src/views/buyer/Search.vue
@@ -2,18 +2,18 @@
   <b-container class="my-5">
     <!-- Buscar -->
     <div class="mb-5">
-      <h1 class="h1">Buscar Productos</h1>
+      <h1 class="h1">{{ $t("search.searchProducts") }}</h1>
       <b-card>
         <b-form-input id="query" name="query" v-model="query"></b-form-input>
         <b-button block variant="primary" @click="handleSearch">
-          Buscar
+          {{ $t("search.beginSearch") }}
         </b-button>
       </b-card>
     </div>
 
     <!-- Resultados -->
     <div class="mb-5">
-      <h2 class="h3">Resultados</h2>
+      <h2 class="h3">{{ $t("search.searchResults") }}</h2>
       <ProductListItem
         v-for="item in searchResults"
         :key="item.id"
