bbe96e49a8f05bb9d29e2f7f766031ae922f5508
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-04 13:06:03 -0300

    extract cart button to component

---
+++ src/components/NavbarShoppingCart.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-nav-item to="/cart" v-if="$store.state.auth.type === 'buyer'">
+    Carrito {{ cartLengthMessage }}
+  </b-nav-item>
+</template>
+
+<script>
+export default {
+  name: "NavbarShoppingCart",
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters["auth/isAuthenticated"];
+    },
+    cart() {
+      return this.$store.state.buyer.shoppingCartItems;
+    },
+    cartLengthMessage() {
+      if (!this.cart.length) {
+        return "vacío";
+      }
+      return `(${this.cart.length})`;
+    }
+  },
+  methods: {
+    handleLogout() {
+      this.$store.dispatch("auth/logout");
+    }
+  }
+};
+</script>
