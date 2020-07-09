e4f01f28695c76948897828f32971f97f25499ad
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-09 19:11:42 -0300

    log actions

--- src/store/auth.js
+++ src/store/auth.js
@@ -8,27 +8,31 @@ export default {
   },
   mutations: {
     setLogin(state, { name, type }) {
+      console.log("auth/setLogin mutation", { name, type });
       state.name = name;
       state.type = type;
     },
     setLogout(state) {
+      console.log("auth/setLogout mutation");
       state.name = null;
       state.type = "";
     }
   },
   actions: {
     login({ commit }, payload) {
-      console.log(payload);
+      console.log("auth/login action", payload);
       commit("setLogin", payload);
       router.push("/dashboard");
     },
     logout({ commit }) {
+      console.log("auth/logout action");
       commit("setLogout");
       router.replace("/");
     }
   },
   getters: {
     isAuthenticated(state) {
+      console.log("auth/isAuthenticated getter");
       return !!state.type;
     }
   }
