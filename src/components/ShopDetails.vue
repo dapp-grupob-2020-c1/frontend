0fb935ee4079349b15a20902f4ccb02aecb8b829
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-12 23:03:49 -0300

    use details component

---
+++ src/components/ShopDetails.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="shop-details">
+    <h2 class="h3">{{ shop.name }}</h2>
+    <p>{{ shop.location.address }}</p>
+    <p>{{ $t("shop.deliveryRadius") }}: {{ shop.deliveryRadius }} Km</p>
+
+    <div class="shop-categories" v-if="expanded">
+      <h3 class="h6">{{ $t("shop.categories") }}</h3>
+      <p>
+        <b-badge
+          variant="secondary"
+          v-for="category in shop.categories"
+          :key="category"
+        >
+          {{ category }}
+        </b-badge>
+      </p>
+
+      <h3 class="h6">{{ $t("shop.paymentMethods") }}</h3>
+      <p>
+        <b-badge
+          variant="secondary"
+          v-for="paymentMethod in shop.paymentMethods"
+          :key="paymentMethod"
+          >{{ paymentMethod }}</b-badge
+        >
+      </p>
+
+      <h3 class="h6">{{ $t("shop.openingDays") }}</h3>
+      <p>
+        {{ shop.openingHour }}-{{ shop.closingHour }},
+        <b-badge variant="secondary" v-for="day in shop.days" :key="day">{{
+          day
+        }}</b-badge>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ShopDetails",
+  props: {
+    shop: {
+      type: Object,
+      required: true
+    },
+    expanded: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
--- src/views/shops/List.vue
+++ src/views/shops/List.vue
@@ -17,76 +17,19 @@
 
     <b-list-group v-if="shopsList.length">
       <b-list-group-item v-for="shop in shopsList" :key="shop.id">
-        <div class="row">
-          <div class="col-4 align-self-center">
-            <GmapMap
-              :center="{
-                lat: shop.location.latitude,
-                lng: shop.location.longitude
-              }"
-              :zoom="15"
-              :options="{
-                disableDefaultUI: true,
-                fullscreenControl: false
-              }"
-              style="width: 100%; height: 200px"
-            >
-              <GmapMarker
-                :position="{
-                  lat: shop.location.latitude,
-                  lng: shop.location.longitude
-                }"
-              />
-            </GmapMap>
-          </div>
-          <div class="col">
-            <h2 class="h3">{{ shop.name }}</h2>
-            <p>{{ shop.location.address }}</p>
-            <p>{{ $t("shop.deliveryRadius") }}: {{ shop.deliveryRadius }} Km</p>
+        <ShopDetails :shop="shop" />
 
-            <h3 class="h6">{{ $t("shop.categories") }}</h3>
-            <b-badge
-              variant="secondary"
-              v-for="category in shop.categories"
-              :key="category"
-            >
-              {{ category }}
-            </b-badge>
-
-            <h3 class="h6">{{ $t("shop.paymentMethods") }}</h3>
-            <p>
-              <b-badge
-                variant="secondary"
-                v-for="paymentMethod in shop.paymentMethods"
-                :key="paymentMethod"
-                >{{ paymentMethod }}</b-badge
-              >
-            </p>
-
-            <h3 class="h6">{{ $t("shop.openingDays") }}</h3>
-            <p>
-              {{ shop.openingHour }}-{{ shop.closingHour }},
-              <b-badge
-                variant="secondary"
-                v-for="day in shop.days"
-                :key="day"
-                >{{ day }}</b-badge
-              >
-            </p>
-
-            <div class="actions my-2">
-              <b-button class="mr-2" :to="`/shops/${shop.id}`">
-                Detalles
-              </b-button>
-              <b-button
-                class="mr-2"
-                variant="outline-danger"
-                @click="handleShopDelete(shop)"
-              >
-                Eliminar
-              </b-button>
-            </div>
-          </div>
+        <div class="actions my-2">
+          <b-button class="mr-2" :to="`/shops/${shop.id}`">
+            Detalles
+          </b-button>
+          <b-button
+            class="mr-2"
+            variant="outline-danger"
+            @click="handleShopDelete(shop)"
+          >
+            Eliminar
+          </b-button>
         </div>
       </b-list-group-item>
     </b-list-group>
@@ -106,9 +49,10 @@
 import ErrorAlert from "../../components/ErrorAlert";
 import { deleteShopRequest } from "../../api/shopRequests";
 import { getShopsRequest } from "../../api/userRequests";
+import ShopDetails from "../../components/ShopDetails";
 export default {
   name: "ShopsList",
-  components: { ErrorAlert },
+  components: { ShopDetails, ErrorAlert },
   data() {
     return {
       requestInfo: {
