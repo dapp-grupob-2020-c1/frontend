fa8ef17e3d9a7520614db02bf26e39cda6ba4562
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-13 01:07:49 -0300

    working search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,15 +1,41 @@
 <template>
   <b-container class="my-5">
-    <!-- Buscar -->
-    <div class="mb-5">
-      <h1 class="h1">{{ $t("search.searchProducts") }}</h1>
-      <b-card>
-        <b-form-input id="query" name="query" v-model="query"></b-form-input>
-        <b-button block variant="primary" @click="handleSearch">
-          {{ $t("search.beginSearch") }}
-        </b-button>
-      </b-card>
-    </div>
+    <b-breadcrumb>
+      <b-breadcrumb-item to="/dashboard">
+        {{ $t("dashboard.dashboard") }}
+      </b-breadcrumb-item>
+      <b-breadcrumb-item active to="/search">
+        {{ $t("search.searchProducts") }}
+      </b-breadcrumb-item>
+    </b-breadcrumb>
+
+    <h1 class="h1">
+      {{ $t("search.searchProducts") }}
+    </h1>
+
+    <ErrorAlert :request-info="requestInfo" />
+
+    <!-- Search -->
+    <b-card>
+      <form @submit.prevent="handleSearch">
+        <b-input-group>
+          <b-form-input
+            id="query"
+            name="query"
+            v-model="searchQuery"
+          ></b-form-input>
+          <template v-slot:append>
+            <b-button block variant="primary" type="submit">
+              {{ $t("search.beginSearch") }}
+            </b-button>
+          </template>
+        </b-input-group>
+        <div class="search-options mt-3">
+          <h3 class="h5">{{ $t("search.searchOptions") }}</h3>
+          <p>Opciones</p>
+        </div>
+      </form>
+    </b-card>
 
     <!-- Resultados -->
     <div class="mb-5">
@@ -24,23 +50,48 @@
 </template>
 
 <script>
+import { searchProductsRequest } from "../api/productsRequests";
 import ProductListItem from "../components/ProductListItem";
+import ErrorAlert from "../components/ErrorAlert";
 export default {
   name: "Search",
-  components: { ProductListItem },
+  components: { ErrorAlert, ProductListItem },
   data() {
     return {
-      query: ""
+      requestInfo: {
+        loading: false,
+        error: false,
+        errorMessageKey: "",
+        errorAdditionalInfo: ""
+      },
+      searchQuery: "",
+      searchResults: []
     };
   },
-  computed: {
-    searchResults() {
-      return this.$store.state.buyer.searchResults;
-    }
-  },
   methods: {
-    handleSearch() {
-      this.$store.dispatch("buyer/beginSearch", this.query);
+    async handleSearch() {
+      const firstLocation = 3;
+      const searchParams = {
+        locationId: firstLocation,
+        keyword: this.searchQuery
+      };
+
+      // reset loading state
+      this.requestInfo = {
+        loading: true,
+        error: false,
+        errorMessageKey: "",
+        errorAdditionalInfo: ""
+      };
+
+      try {
+        const response = await searchProductsRequest(searchParams);
+        this.searchResults = response.data;
+      } catch (error) {
+        this.requestInfo.error = true;
+      } finally {
+        this.requestInfo.loading = false;
+      }
     }
   }
 };
