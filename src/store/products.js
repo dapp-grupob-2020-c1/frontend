f4e5b5d19fa18075157f84214c9be55077431dd0
tehuel <torresbaldi.tehuel@gmail.com>
2020-07-19 20:31:52 -0300

    createMany

--- src/store/products.js
+++ src/store/products.js
@@ -1,5 +1,6 @@
 import { getShopRequest } from "../api/shopRequests";
 import {
+  createManyProductsRequest,
   createProductRequest,
   deleteProductRequest
 } from "../api/productsRequests";
@@ -21,9 +22,6 @@ export default {
         const httpClient = rootState.auth.httpClient;
         const getShopResponse = await getShopRequest(httpClient, shopId);
         commit("setProducts", getShopResponse.data.products);
-        dispatch("messages/showMessage", "user.getShopProductsSuccess", {
-          root: true
-        });
       } catch (error) {
         commit("requests/setError", null, { root: true });
         dispatch("messages/showErrorMessage", "user.getShopProductsError", {
@@ -55,6 +53,34 @@ export default {
       } catch (error) {
         commit("requests/setError", null, { root: true });
         dispatch("messages/showErrorMessage", "user.createProductError", {
+          root: true
+        });
+        // handle different error types
+        if (error.response) {
+          commit("requests/setError", "app.responseError", { root: true });
+        } else if (error.request) {
+          commit("requests/setError", "app.connectionError", { root: true });
+        }
+      } finally {
+        commit("requests/endLoading", null, { root: true });
+      }
+    },
+    async createManyProducts({ commit, dispatch, rootState }, productsInfo) {
+      console.log("createManyProducts action", productsInfo);
+      commit("requests/beginLoading", null, { root: true });
+      try {
+        const httpClient = rootState.auth.httpClient;
+        const createProductResponse = await createManyProductsRequest(
+          httpClient,
+          productsInfo
+        );
+        console.log(createProductResponse);
+        dispatch("messages/showMessage", "user.createManyProductsSuccess", {
+          root: true
+        });
+      } catch (error) {
+        commit("requests/setError", null, { root: true });
+        dispatch("messages/showErrorMessage", "user.createManyProductsError", {
           root: true
         });
         // handle different error types
